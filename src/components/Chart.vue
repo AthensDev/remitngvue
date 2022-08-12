3b8f05303be0ef3f867e80caa678623dc8b5d939
samurai1979 <xaiyax01@gmail.com>
2022-08-12 01:25:04 +0100

    worked on dashboard

---
+++ src/components/Chart.vue
@@ -0,0 +1,103 @@
+<template>
+  <Line
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale
+)
+
+export default {
+  name: 'LineChart',
+  components: { Line },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 150
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+      }
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      chartData: {
+         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+            {
+            label: "Received",
+            backgroundColor: "#f87979",
+            borderColor: "#f87979",
+            pointBackgroundColor: "#fff",
+            borderWidth: 1,
+            pointBorderColor: "#f87979",
+            data: [4000, 3900, 1000, 4000, 3912, 8000, 4050]
+            },
+            {
+            label:"Sent",
+            backgroundColor: "#00a79d",
+            borderColor: "#00a79d",
+            pointBackgroundColor: "#fff",
+            borderWidth: 1,
+            pointBorderColor: "#00a79d",
+            data: [6000, 2900, 7000, 9000, 1912, 5000, 2050]
+            }
+      ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -12,7 +12,7 @@
             <div class="convert-box">
                 <ul class="convert-tablist list-style" role="tablist">
                     <li style="width: 33%" class="nav-item">
-                        <a class="active" href="/?step=1"><i class="ri-exchange-dollar-line"></i>History</a>
+                        <a class="active" href="/?step=1"><i class="ri-dashboard-line"></i>Dashboard</a>
                     </li>
                     <!--   <li style="width: 33%" class="nav-item">
                         <a href="/?step=2"><i class="ri-send-plane-line"></i>Confirm</a>
@@ -26,7 +26,19 @@
                     <section class="exchange-table-wrap pb-75">
                         <div class="container">
                             <div class="row gx-5">
-                                <div class="col-lg-12">
+                                <h2 class="welcome-heading">Welcome {{ user.first_name }},</h2>
+                                <Chart />
+                                <div class="card-container">
+                                    <div class="total-received-card">
+                                        <h3>Total Received</h3>
+                                        <h1>60,000</h1>
+                                    </div>
+                                    <div class="total-sent-card">
+                                        <h3>Total Sent</h3>
+                                        <h1>60,000</h1>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 mt-5">
                                     <div class="section-title style1 text-center mb-25">
                                         <h2>Recent Transactions</h2>
                                     </div>
@@ -101,12 +113,14 @@ export default {
             router
         }
     },
-    data() {
-        return {
-            user: {}
-        }
+    // data() {
+    //     return {
+    //         user: {}
+    //     }
+    // },
+    computed: {
+        ...mapGetters("user", ["user"]),
     },
-    computed: {},
     mounted() {},
     methods: {
         ...mapActions('auth', ['login', 'logout']),
@@ -122,3 +136,46 @@ export default {
 }
 
 </script>
+<style scoped="scoped">
+    .theme-dark .welcome-heading {
+        color: #fff;
+        font-size: 22px;
+    }
+    .theme-light .welcome-heading {
+        color: #333;
+        font-size: 22px;
+    }
+    .card-container {
+        margin: 30px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+    .card-container div {
+        border-radius: 15px;
+        padding: 15px;
+        width: 48%;
+    }
+    .theme-light .card-container div{
+        border: 1px solid #ccc;
+    }
+    .theme-dark .card-container div{
+        border: 1px solid #fff;
+    }
+    .theme-light .card-container h3 {
+        font-size: 16px;
+        color: #333;
+    }
+    .theme-dark .card-container h3 {
+        font-size: 16px;
+        color: #fff;
+    }
+    .card-container h1 {
+        font-size: 28px;
+    }
+    .total-received-card h1 {
+        color: green !important;
+    }
+    .total-sent-card h1 {
+        color: red !important;
+    }
+</style>
